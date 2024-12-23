6599c4c0fb52ddcedb92353db99f4c4861bbded0
Mike Wilson <youroldmangaming@gmail.com>
2024-12-24 03:22:06 +1300

    Update install.sh

--- cocalc/install.sh
+++ cocalc/install.sh
@@ -2,3 +2,8 @@
  docker run --name=cocalc -d -v ~/cocalc:/projects -p 443:443 sagemathinc/cocalc-docker
 
  https://gitee.com/microcloud/cocalc-docker
+
+ docker exec -it cocalc bash
+root@931045eda11f:/# make-user-admin wstein@gmail.com
+
+ 
